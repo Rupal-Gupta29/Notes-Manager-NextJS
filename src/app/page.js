7672bcbbaa675a8cd6deb5049647aa3bf66061eb
Rupal <rupalgupta951@gmail.com>
2026-06-05 18:34:28 +0530

    update next url

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,13 @@ import AddNote from "@/components/appComponents/AddNote";
 import NoteList from "@/components/appComponents/NoteList";
 
 async function fetchAllNotes() {
-  let apiResponse = await fetch("http://localhost:3000/api/get-notes", {
-    method: "GET",
-    cache: "no-store",
-  });
+  let apiResponse = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/get-notes`,
+    {
+      method: "GET",
+      cache: "no-store",
+    },
+  );
   let noteList = await apiResponse.json();
   return noteList?.data;
 }
